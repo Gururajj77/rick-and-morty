181b684f7baf7ddefc1c1920a283d1970cbc2d30
Gururajj77 <gururajtakeshi@gmail.com>
2023-12-20 00:55:21 +0530

    tests updated again

--- src/tests/CharacterCard.test.tsx
+++ src/tests/CharacterCard.test.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { render, fireEvent, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import CharacterCard from "../components/Characters/CharacterCard/CharacterCard";
